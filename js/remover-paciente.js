1ebea6fa61dbafa740861595e30cb500cf8655de
ElJohnnie <johnfollmer1995@gmail.com>
2021-08-09 14:46:23 -0300

    setando um setTimeout

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,14 @@
-var tabela = document.querySelector("table");
-
 //o escutador é o table mas o evento é o campo do pai escolhido, mas o que vamos remover é o pai do td que é o tr
+var tabela = document.querySelector("#tabela-pacientes");
+
 tabela.addEventListener("dblclick", function(event) {
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fadeOut");
+
+    //Quando queremos aguardar um tempo, devemos usar a função setTimeout. Será passada como parâmetro uma função anônima com quanto tempo deve ser aguardado:
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
+
 });
 
 //metodo antigo
