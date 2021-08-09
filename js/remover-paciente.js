699b528b34181cf1020f6a508d247198d8b6e0a1
ElJohnnie <johnfollmer1995@gmail.com>
2021-08-09 14:37:32 -0300

    remover event.target.parentNote

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,15 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        this.remove();
-    });
+//o escutador é o table mas o evento é o campo do pai escolhido, mas o que vamos remover é o pai do td que é o tr
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.remove();
 });
+
+//metodo antigo
+//var pacientes = document.querySelectorAll(".paciente");
+
+//pacientes.forEach(function(paciente) {
+//  paciente.addEventListener("dblclick", function() {
+//    this.remove();
+//});
+//});
